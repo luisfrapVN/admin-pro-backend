65f106c08b8fbcf29e41b646179cf2b1c97270e1
luisfrapVN <lfraile@viewnext.com>
2021-02-18 12:07:06 +0100

    Fin seccion 12

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,9 @@
-const { response } = require("express");
+const { response, request } = require("express");
 const bcrypt = require("bcryptjs");
 
 const Usuario = require("../models/usuario");
 const { generarJWT } = require("../helpers/jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req, res = response) => {
   const { email, password } = req.body;
@@ -19,22 +20,20 @@ const login = async (req, res = response) => {
 
     //Verificar contraseña
     const validPassword = bcrypt.compareSync(password, usuarioDB.password);
-    if(!validPassword){
-        return res.status(400).json({
-            ok:false, 
-            msg:"No se encontro mail/contraseña"
-        })
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: "No se encontro mail/contraseña",
+      });
     }
-
 
     //Generar el token - jwt
 
-const token = await generarJWT(usuarioDB.id)
-
+    const token = await generarJWT(usuarioDB.id);
 
     res.json({
       ok: true,
-      token
+      token,
     });
   } catch (error) {
     console.log(error);
@@ -45,6 +44,48 @@ const token = await generarJWT(usuarioDB.id)
   }
 };
 
+const googleSignIn = async (req, res = response) => {
+  const googleToken = req.body.token;
+
+  try {
+    const { name, email, picture } = await googleVerify(googleToken);
+
+    const usuarioDB = await Usuario.findOne({ email });
+    let usuario;
+    if (!usuarioDB) {
+      usuario = new Usuario({
+        nombre: name,
+        email,
+        password: "@@@",
+        img: picture,
+        google: true,
+      });
+    } else {
+      //existe usuario
+      usuario = usuarioDB;
+      usuario.google = true;
+      usuario.password = "@@@";
+    }
+
+    //Guardar en BD
+    await usuario.save();
+
+    //Generar el token - jwt
+    const token = await generarJWT(usuarioDB.id);
+
+    res.json({
+      ok: true,
+      token
+    });
+  } catch (error) {
+    res.status(401).json({
+      ok: false,
+      msg: "Error token",
+    });
+  }
+};
+
 module.exports = {
   login,
+  googleSignIn,
 };

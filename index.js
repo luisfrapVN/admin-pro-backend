0767f9173f3f7b1ef58f969adcd2ad5a1ffef1b8
luisfrapVN <lfraile@viewnext.com>
2021-02-22 18:41:22 +0100

    SPA control

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const path = require("path");
 const express = require("express"); //Similar a hacer una importacion
 const cors = require("cors");
 
@@ -27,6 +28,11 @@ app.use("/api/medicos", require("./routes/medicos"));
 app.use("/api/login", require("./routes/auth"));
 app.use("/api/todo", require("./routes/busquedas"));
 app.use("/api/upload", require("./routes/uploads"));
+
+//Lo ultimo
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public/index.html"));
+});
 
 app.listen(process.env.PORT, () => {
   console.log("Servidor corriendo en puerto " + process.env.PORT);
